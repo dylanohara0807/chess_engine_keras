207432de75bec09134c5f29f73dcc5b8aa1169f9
dylanohara0807 <dylanohara0807@yahoo.com>
2023-05-01 21:49:07 -0400

    Added multiprocessing to minimax.

--- test.py
+++ test.py
@@ -1,67 +1,105 @@
+import sys
+import time
 import chess
 import cengine
-import random
+import numpy as np
+from tensorflow import keras
+from multiprocessing import Pool
 
-def predict_pos(fen_s):
+class datap():
 
-    board_li = [[0 for j in range(66)] for i in range(1)]
-    m_c = fen_s[fen_s.index(" ") + 1 : fen_s.index(" ") + 2]
-    fen_s = fen_s[0 : fen_s.index(" ")]
-    board_li[0] = cengine.gen_board(fen_s)
-    board_li[0][64] = -1 if m_c == "b" else 1
+    def __init__(self, s, d, i, a, b, m):
+        self.state = s; self.depth = d; self.ismax = i; self.alpha = a; self.beta = b; self.model = m
 
-    return board_li[0][65] + random.randint(-2, 2) / 4
+    state: None
+    depth: int
+    ismax: bool
+    alpha: int
+    beta: int
+    model: None
 
-def beam_search(state, depth, ismax):
+def possible_new_states(state):
 
-    backtrack = {}
-    eval = {}
-    pred = None
-    
-    for nm in state.legal_moves:
-
-        state.push(nm)
-        new_state = state.copy()
-        res = 0; count = 0
-
-        for lm in new_state.legal_moves:
-
-            new_state.push(lm)
-            last_state = new_state.copy()
-            res += (predict_pos(last_state.fen())) ** 2
-            count += 1
-            new_state.pop()
-
-        eval[str(nm) + "!"] = [res / count, new_state]
+    ret = []
+    for i in state.legal_moves:
+        state.push(i)
+        ret.append(state.copy())
         state.pop()
+
+    return ret
+
+def beam_minimax(pack):
+
+    def predict_pos(fen_s):
+
+        board_li = [[0 for j in range(66)] for i in range(1)]
+        m_c = fen_s[fen_s.index(" ") + 1 : fen_s.index(" ") + 2]
+        fen_s = fen_s[0 : fen_s.index(" ")]
+        board_li[0] = cengine.gen_board(fen_s)
+        board_li[0][64] = -1 if m_c == "b" else 1
+
+        return pack.model.predict(board_li, verbose = 0) + board_li[0][65] / 2
+
+    alpha = pack.alpha; beta = pack.beta; is_maximizing = pack.ismax; depth = pack.depth; state = pack.state
+
+    if pack.depth == 0:
+        return predict_pos(pack.state.fen())
     
-    r_moves = sorted(eval.keys(), key = lambda x: eval[x][0], reverse = True if ismax else False)[ : 4]
-    r_values =  sorted(eval.values(), key = lambda x: x[0])[ : 4]
-    q = []
-    for i in range(4): 
-        backtrack[r_moves[i]] = pred
-        q.append([r_moves[i], r_values[i]])
+    if is_maximizing:
 
-    return q
-
+        bV = -30
+        q = []
+        for new_state in possible_new_states(state):
+            q.append([predict_pos(new_state.fen()), new_state])
+        q.sort(reverse=True, key = lambda x: x[0])
+        for i in range(min(depth, len(q))):
+            value = beam_minimax(datap(q[i][1], depth - 1, False, alpha, beta, pack.model))
+            bV = max(bV, value) 
+            alpha = max(alpha, bV)
+            if beta <= alpha:
+                break
+        return bV
+    
+    else:
+        
+        bV = 30
+        q = []
+        for new_state in possible_new_states(state):
+            q.append([predict_pos(new_state.fen()), new_state])
+        q.sort(key = lambda x: x[0])
+        for i in range(min(depth, len(q))):
+            value = beam_minimax(datap(q[i][1], depth - 1, True, alpha, beta, pack.model))
+            bV = min(bV, value) 
+            beta = min(beta, bV)
+            if beta <= alpha:
+                break
+        return bV
 
 def main():
 
     board = chess.Board()
-    global posmem; global model
-    posmem = {}
     topevals_lf = []
-    #model = keras.models.load_model("model_data/" + sys.argv[1])
+    model = keras.models.load_model("model_data/" + sys.argv[1])
     white_b = True
 
-    print(beam_search(board, 0, white_b))
-
-    """
     while (True):
 
         topevals_lf.clear()
-        posmem.clear()
+
         t = time.perf_counter()
+
+        q = []; mv = []
+        for i in board.legal_moves:
+            board.push(i)
+            mv.append(i)
+            q.append(datap(board.copy(), 4, not white_b, -30, 30, model))
+            board.pop()
+
+        with Pool() as pool:
+            res = pool.map(beam_minimax, q)
+            for p, v in enumerate(res):
+                topevals_lf.append([v, mv[p]])
+
         print("Time Alloted:", time.perf_counter() - t)
 
         topevals_lf.sort(reverse=True, key = lambda x: x[0]) if white_b else topevals_lf.sort(key = lambda x: x[0])
@@ -78,7 +116,6 @@ def main():
             except Exception:
                 print("Invalid move.",end="")
         white_b = False if white_b else True
-        """
 
 if __name__ == "__main__":
     main()
